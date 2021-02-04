9c62bf47a0ec6954ca5a7679864b30ae0e8d7f3e
Edward Huang <ejhuang.2015@gmail.com>
2021-02-04 18:06:08 -0500

    Fixed the Release Date filter

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -62,12 +62,12 @@ $(document).ready(function () {
     const view5Chart = document.querySelector("#view5Chart");
     const view5Question = document.querySelector("#view5Question");
     var view5Data = [
-        { "label": "The 1970s", "value": "1970|1971|1972|1973|1974|1975|1976|1977|1978|1979", "question": "Ready for the 70s? <br> Oh yes, you know you are." },
-        { "label": "The 1980s", "value": "1980|1981|1982|1983|1984|1985|1986|1987|1988|1989", "question": "Ready for the 80s? <br> Oh yes, you know you are." },
-        { "label": "The 1990s", "value": "1990|1991|1992|1993|1994|1995|1996|1997|1998|1999", "question": "Ready for the 90s? <br> Oh yes, you know you are." },
-        { "label": "The 2000s", "value": "2000|2001|2002|2003|2004|2005|2006|2007|2008|2009", "question": "Ready for the 00s? <br> Oh yes, you know you are." },
-        { "label": "The 2010s", "value": "2010|2011|2012|2013|2014|2015|2016|2017|2018|2019", "question": "Ready for the 2010s? <br> Oh yes, you know you are." },
-        { "label": "Most Recent", "value": "2020|2021", "question": "Ready for the most recent stuff? <br> Oh yes, you know you are." }
+        { "label": "The 1970s", "value": "197", "question": "Ready for the 70s? <br> Oh yes, you know you are." },
+        { "label": "The 1980s", "value": "198", "question": "Ready for the 80s? <br> Oh yes, you know you are." },
+        { "label": "The 1990s", "value": "199", "question": "Ready for the 90s? <br> Oh yes, you know you are." },
+        { "label": "The 2000s", "value": "200", "question": "Ready for the 00s? <br> Oh yes, you know you are." },
+        { "label": "The 2010s", "value": "201", "question": "Ready for the 2010s? <br> Oh yes, you know you are." },
+        { "label": "Most Recent", "value": "202", "question": "Ready for the most recent stuff? <br> Oh yes, you know you are." }
     ];
 
 // References to all of the different views
@@ -388,7 +388,18 @@ $(document).ready(function () {
                 movieGenreID = data.value;
                 break;
             case '5':
-                movieReleaseYear = data.value;
+                if (data.value == 202) {
+                    // Random number from 0-1
+                    var randomNumber = Math.floor(Math.random() * (2));
+                    movieReleaseYear = data.value + randomNumber.toString();
+                    console.log(movieReleaseYear);
+                }
+                else {
+                    // Random number from 0-9
+                    var randomNumber = Math.floor(Math.random() * (10));
+                    movieReleaseYear = data.value + randomNumber.toString();
+                    console.log(movieReleaseYear);
+                }
                 getMovie();
                 break;
         }
